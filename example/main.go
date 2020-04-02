865eb1b368e0ced344c79f30a85fe81d8a34bf40
Alexandre NEGREL <negrel.dev@protonmail.com>
2020-04-02 22:02:37 +0200

    example test

--- example/main.go
+++ example/main.go
@@ -1,9 +1,51 @@
 package main
 
 import (
-	"github.com/negrel/ginger"
+	"log"
+	"time"
+
+	"github.com/gdamore/tcell"
+	"github.com/negrel/ginger/v1"
+	"github.com/negrel/ginger/v1/widget"
 )
 
 func main() {
-	app := ginger.New()
+	app, err := ginger.New()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.Start(&ginger.Activity{
+		Root: &widget.Column{
+			Childrens: []widget.Widget{
+				&widget.Row{
+					Childrens: []widget.Widget{
+						&widget.Text{
+							Content: "- ",
+						},
+						&widget.Text{
+							Content: "Hello, how are you ?",
+							Colors:  tcell.StyleDefault.Background(tcell.ColorPaleGreen),
+						},
+					},
+				},
+				&widget.Row{
+					Childrens: []widget.Widget{
+						&widget.Text{
+							Content: "- ",
+						},
+						&widget.Text{
+							Content: "Fine, fuck you",
+							Colors:  tcell.StyleDefault.Background(tcell.ColorTurquoise),
+						},
+					},
+				},
+			},
+		},
+	})
+
+	time.Sleep(time.Second * 5)
+
+	app.Stop()
 }
