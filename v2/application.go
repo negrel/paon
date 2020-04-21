d9e4e98792e9cf5e166c4705a35572bdda94235e
Alexandre NEGREL <negrel.dev@protonmail.com>
2020-04-21 22:19:29 +0200

    removing cache for layout & improving cache for widgets

--- v2/application.go
+++ v2/application.go
@@ -1,18 +1,22 @@
 package ginger
 
 import (
-	"fmt"
 	"image"
 	"log"
 
 	"github.com/gdamore/tcell"
 	"github.com/negrel/ginger/v2/render"
 	"github.com/negrel/ginger/v2/widgets"
+	"github.com/negrel/ginger/v2/widgets/events"
 )
+
+var _ events.ResizeListener = &Application{}
 
 // Application is the entry point of your terminal
 // application
 type Application struct {
+	devMode bool
+
 	screen   tcell.Screen
 	renderer chan *render.Frame
 	root     *widgets.Root
@@ -20,7 +24,7 @@ type Application struct {
 
 // New return a new Application
 func New(child widgets.Widget) *Application {
-	screen, err := tcell.NewTerminfoScreen()
+	screen, err := tcell.NewScreen()
 
 	if err != nil {
 		log.Fatal(err)
@@ -32,82 +36,62 @@ func New(child widgets.Widget) *Application {
 		root:     widgets.ROOT(child),
 	}
 
+	events.Emitter.AddResizeListener(app)
+
 	return app
 }
 
+/*****************************************************
+ ********************* Methods ***********************
+ *****************************************************/
+// ANCHOR Methods
+
 // Start the application
 func (a *Application) Start() error {
-	defer func() {
-		if r := recover(); r != nil {
-			a.screen.Fini()
-			fmt.Println(r)
-		}
-	}()
-
 	err := a.screen.Init()
 
 	if err != nil {
 		return err
 	}
 
-	// Listening to events
-	go a.listen()
-
 	// Renderer
-	render := render.Renderer{
+	renderer := render.Renderer{
 		Input:   a.renderer,
 		Paint:   a.paint,
 		Refresh: a.screen.Show,
 	}
 
-	go render.Start()
+	go renderer.Start()
 
-	// First paint
-	a.Show()
+	// Listening to events
+	go a.listen()
 
 	return nil
 }
 
-// Show force the total redraw of the application.
-func (a *Application) Show() {
-	width, height := a.screen.Size()
+// OnResize implements events.ResizeListener interface.
+func (a *Application) OnResize(re *events.ResizeEvent) {
+	a.ForceRender(re.Width(), re.Height())
+}
 
-	a.renderer <- a.root.Render(widgets.Constraint{
-		Bounds: image.Rect(0, 0, width, height),
-		// Inherited: &style.DefaultTheme,
-	})
+// ForceRender force the total redraw of the application.
+func (a *Application) ForceRender(width, height int) {
+	frame := a.root.Rendering(image.Rect(0, 0, width, height))
+
+	a.renderer <- frame
 }
 
 // paint to the screen the raw cell of the render
 func (a *Application) paint(rc *render.RawCell) {
-	a.screen.SetContent(rc.X, rc.Y, rc.Mainc, rc.Combc, rc.Style)
+	// log.Printf("%v %v %v %v", rc.X, rc.Y, rc.Mainc, rc.Style)
+	a.screen.SetContent(rc.X, rc.Y, rc.Mainc, []rune{}, rc.Style)
 }
 
 // listen to the terminal events
 func (a *Application) listen() {
 	for {
 		event := a.screen.PollEvent()
-		switch event := event.(type) {
-
-		case *tcell.EventKey:
-			log.Printf("%+v %v\n", event.Name(), event.Key())
-
-		case *tcell.EventMouse:
-			log.Printf("%+v\n", event)
-
-		case *tcell.EventResize:
-			log.Printf("%+v\n", event)
-			a.Show()
-
-		case *tcell.EventTime:
-			log.Printf("%+v\n", event)
-
-		case *tcell.EventInterrupt:
-			log.Printf("%+v\n", event)
-
-		case *tcell.EventError:
-			log.Printf("%+v\n", event)
-		}
+		events.Emit <- event
 	}
 }
 
