66be0cf47aab17b649be41b8dd6252254acb5e59
Alexandre NEGREL <negrel.dev@protonmail.com>
2020-04-26 00:12:59 +0200

    adding activity

--- v2/application.go
+++ v2/application.go
@@ -1,103 +1,167 @@
 package ginger
 
 import (
+	"errors"
 	"image"
 	"log"
 
 	"github.com/gdamore/tcell"
+	"github.com/negrel/ginger/v2/events"
 	"github.com/negrel/ginger/v2/render"
-	"github.com/negrel/ginger/v2/widgets"
-	"github.com/negrel/ginger/v2/widgets/events"
 )
 
-var _ events.ResizeListener = &Application{}
+var renderer chan *render.Frame
+
+func init() {
+	renderer = make(chan *render.Frame)
+}
 
 // Application is the entry point of your terminal
 // application
 type Application struct {
-	devMode bool
-
 	screen   tcell.Screen
-	renderer chan *render.Frame
-	root     *widgets.Root
+	activity *Activity
 }
 
 // New return a new Application
-func New(child widgets.Widget) *Application {
+func New() (*Application, error) {
 	screen, err := tcell.NewScreen()
-
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	app := &Application{
-		screen:   screen,
-		renderer: make(chan *render.Frame),
-		root:     widgets.ROOT(child),
+		screen: screen,
 	}
 
-	events.Emitter.AddResizeListener(app)
+	err = app.screen.Init()
+	if err != nil {
+		return nil, err
+	}
 
-	return app
+	// init the renderer
+	renderer := &render.Renderer{
+		Input:   renderer,
+		Paint:   app.paint,
+		Refresh: app.screen.Show,
+	}
+	go renderer.Start()
+	// start listening to events
+	go app.listen()
+
+	return app, nil
 }
-
-/*****************************************************
- ********************* Methods ***********************
- *****************************************************/
-// ANCHOR Methods
 
 // Start the application
-func (a *Application) Start() error {
-	err := a.screen.Init()
+func (a *Application) Start(ac *Activity) {
+	defer func() {
+		if p := recover(); p != nil {
+			if a.screen != nil {
+				a.screen.Fini()
+			}
+			log.Panic(p)
+		}
+	}()
 
-	if err != nil {
-		return err
+	// First activity started
+	if a.activity != nil {
+		a.activity.stop()
 	}
 
-	// Renderer
-	renderer := render.Renderer{
-		Input:   a.renderer,
-		Paint:   a.paint,
-		Refresh: a.screen.Show,
-	}
-
-	go renderer.Start()
-
-	// Listening to events
-	go a.listen()
-
-	return nil
+	a.activity = ac
+	a.activity.start(a.screen.Size)
 }
 
-// OnResize implements events.ResizeListener interface.
-func (a *Application) OnResize(re *events.ResizeEvent) {
-	a.ForceRender(re.Width(), re.Height())
-}
-
-// ForceRender force the total redraw of the application.
-func (a *Application) ForceRender(width, height int) {
-	frame := a.root.Rendering(image.Rect(0, 0, width, height))
-
-	a.renderer <- frame
-}
-
-// paint to the screen the raw cell of the render
-func (a *Application) paint(rc *render.RawCell) {
-	// log.Printf("%v %v %v %v", rc.X, rc.Y, rc.Mainc, rc.Style)
-	a.screen.SetContent(rc.X, rc.Y, rc.Mainc, []rune{}, rc.Style)
-}
-
-// listen to the terminal events
-func (a *Application) listen() {
-	for {
-		event := a.screen.PollEvent()
-		events.Emit <- event
-	}
-}
-
-// Stop the application
+// Stop the application.
 func (a *Application) Stop() error {
+	a.activity.stop()
 	a.screen.Fini()
 
 	return nil
+}
+
+// EnableMouse enable the mouse support.
+func (a *Application) EnableMouse() error {
+	if !a.screen.HasMouse() {
+		return errors.New("mouse is not supported by your terminal")
+	}
+
+	a.screen.EnableMouse()
+	return nil
+}
+
+// listen to the terminal events and send
+// them to the events emitter.
+func (a *Application) listen() {
+	for {
+		events.Emit <- adaptEvent(a.screen.PollEvent())
+	}
+}
+
+// oldScreenSize is the old screen size for comparing to recent event.
+var oldScreenSize events.Size = events.Size{
+	Width:  0,
+	Height: 0,
+}
+
+// Adapt the tcell event for the events emitter (ginger event).
+func adaptEvent(event tcell.Event) events.Event {
+	switch rawEvent := event.(type) {
+	case *tcell.EventKey:
+		log.Printf("Key event: %+v %v\n", rawEvent.Name(), rawEvent.Key())
+		direction := events.Direction(rawEvent.Key() - 257)
+
+		se := events.NewScrollEvent(rawEvent.When(), direction)
+
+		return se
+
+	case *tcell.EventMouse:
+		log.Printf("Mouse event: %+v\n", rawEvent)
+		buttons := rawEvent.Buttons()
+		// scrollDir := events.Direction(buttons << 8)
+		click := events.Button(0xFF & buttons)
+
+		// scroll event
+		// if scrollDir > 0 {
+		// 	return events.NewScrollEvent(rawEvent.When(), scrollDir)
+		// }
+		// click event
+		X, Y := rawEvent.Position()
+		return events.NewClickEvent(rawEvent.When(), click, image.Pt(X, Y))
+
+	case *tcell.EventResize:
+		// log.Printf("Resize event: %+v\n", rawEvent)
+		w, h := rawEvent.Size()
+		newSize := events.Size{
+			Width:  w,
+			Height: h,
+		}
+		re := events.NewResizeEvent(rawEvent.When(), newSize, oldScreenSize)
+
+		// Update oldScreenSize
+		oldScreenSize = newSize
+
+		return re
+
+	case *tcell.EventError:
+		log.Printf("Error event: %+v\n", rawEvent)
+		return nil
+
+	case *tcell.EventInterrupt:
+		log.Printf("Interrupt event: %+v\n", rawEvent)
+		return nil
+
+	case *tcell.EventTime:
+		log.Printf("Time event: %+v\n", rawEvent)
+		return nil
+
+	default:
+		return nil
+	}
+}
+
+// paint is passed to the renderer for painting raw cells.
+func (a *Application) paint(rc *render.RawCell) {
+	// log.Printf("%v %v %v %v", rc.X, rc.Y, rc.Mainc, rc.Style)
+	a.screen.SetContent(rc.X, rc.Y, rc.Mainc, []rune{}, rc.Style)
 }
