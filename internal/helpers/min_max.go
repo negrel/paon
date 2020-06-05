da6bc0389417bb5390842eb04a117154b17971a4
Alexandre Negrel <negrel.dev@protonmail.com>
2020-06-05 23:07:33 +0200

    adding some helpers object for positionning and mask

---
+++ internal/helpers/min_max.go
@@ -0,0 +1,19 @@
+package utils
+
+// Min return the smallest integer.
+func Min(x, y int) int {
+	if x < y {
+		return x
+	}
+
+	return y
+}
+
+// Max return the biggest integer.
+func Max(x, y int) int {
+	if x > y {
+		return x
+	}
+
+	return y
+}
---
+++ internal/helpers/point.go
@@ -0,0 +1,42 @@
+package utils
+
+// Infinite represent an infinite value.
+const Infinite int = -1
+
+// Point define a point in Cartesian space a
+// specified distance from a separately-maintained
+// origin. No negative point should be used.
+type Point struct {
+	X, Y int
+}
+
+// Pt is a shortcut for Point.
+func Pt(x, y int) Point {
+	return Point{
+		X: x,
+		Y: y,
+	}
+}
+
+/*****************************************************
+ ********************* Methods ***********************
+ *****************************************************/
+// ANCHOR Methods
+
+// Add translate the point coordinate by adding the
+// given point.
+func (p Point) Add(other Point) Point {
+	p.X += other.X
+	p.Y += other.Y
+
+	return p
+}
+
+// Sub translate the point coordinate by substracting
+//  the given point.
+func (p Point) Sub(other Point) Point {
+	p.X -= other.X
+	p.Y -= other.Y
+
+	return p
+}
