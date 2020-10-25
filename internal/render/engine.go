e9f6cd272b75f2adbc9a3d0e6ed25cce53c8966c
Alexandre Negrel <negrel.dev@protonmail.com>
2020-10-25 20:22:56 +0100

    adding a rendering engine in the internal/render package

---
+++ internal/render/engine.go
@@ -0,0 +1,64 @@
+package render
+
+import (
+	"context"
+	"time"
+
+	"github.com/negrel/debuggo/pkg/log"
+)
+
+// Engine is responsible for rendering the
+type Engine struct {
+	ch          chan Patch
+	ctx         context.Context
+	Surface     Surface
+	ticker      *time.Ticker
+	needRefresh bool
+}
+
+// NewEngine return a new rendering engine that draw on the given surface.
+func NewEngine(surface Surface, ctx context.Context) *Engine {
+	log.Infoln("creating the rendering engine")
+
+	return &Engine{
+		ch:      make(chan Patch),
+		ctx:     ctx,
+		Surface: surface,
+		ticker:  time.NewTicker(time.Millisecond * 16),
+	}
+}
+
+func (e *Engine) Render(patch Patch) {
+	e.ch <- patch
+}
+
+// SetUpdateInterval set the update interval of the surface.
+// By default the rendering engine update the surface every 16ms (around 60fps).
+func (e *Engine) SetUpdateInterval(duration time.Duration) {
+	e.ticker = time.NewTicker(duration)
+}
+
+// Start the rendering engine.
+func (e *Engine) Start() {
+	log.Infoln("starting the rendering engine")
+
+renderLoop:
+	for {
+		select {
+		case patch := <-e.ch:
+			e.Surface.Apply(patch)
+			e.needRefresh = true
+
+		case <-e.ticker.C:
+			if e.needRefresh {
+				e.Surface.Update()
+				e.needRefresh = false
+			}
+
+		case <-e.ctx.Done():
+			break renderLoop
+		}
+	}
+
+	log.Infoln("stopping the rendering engine")
+}
