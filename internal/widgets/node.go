d60be77b3caa420b2ca5fdfc977ebe4fa7e2fc32
Alexandre Negrel <negrel.dev@protonmail.com>
2020-10-25 19:14:07 +0100

    cleaning internal/widgets package

--- internal/widgets/node.go
+++ internal/widgets/node.go
@@ -2,10 +2,14 @@ package widgets
 
 import (
 	"fmt"
+
 	"github.com/google/uuid"
 
 	"github.com/negrel/debuggo/pkg/log"
+
+	"github.com/negrel/paon/internal/render"
 	"github.com/negrel/paon/internal/style"
+	"github.com/negrel/paon/internal/utils"
 )
 
 var _ Widget = &Node{}
@@ -16,10 +20,6 @@ type Node struct {
 	name   string
 	parent Layout
 	style  *style.Node
-}
-
-func (n *Node) slot() int {
-	panic("implement me")
 }
 
 // NewNodeWidget return a new Node object to embed in custom
@@ -36,18 +36,22 @@ func NewNodeWidget(name string) *Node {
 	return n
 }
 
+// Name implements the Widget interface.
 func (n *Node) Name() string {
 	return n.name
 }
 
+// UUID implements the Widget interface.
 func (n *Node) UUID() uuid.UUID {
 	return n.uuid
 }
 
+// String implements the fmt.Stringer interface.
 func (n *Node) String() string {
 	return fmt.Sprintf("%v-%v", n.name, n.uuid.String())
 }
 
+// Parent implements the Widget interface.
 func (n *Node) Parent() Layout {
 	return n.parent
 }
@@ -57,6 +61,16 @@ func (n *Node) adoptedBy(parent Layout) {
 	// TODO update style tree.
 }
 
+// Style implements the Widget interface.
 func (n *Node) Style() *style.Node {
 	return n.style
+}
+
+func (n *Node) slot() int {
+	panic("implement me")
+}
+
+// Render implements the render.Renderable interface.
+func (n *Node) Render(rect utils.Rectangle) render.Patch {
+	panic("implement me")
 }
--- internal/widgets/widget.go
+++ internal/widgets/widget.go
@@ -3,6 +3,8 @@
 package widgets
 
 import (
+	"fmt"
+
 	"github.com/google/uuid"
 
 	"github.com/negrel/paon/internal/render"
@@ -11,16 +13,20 @@ import (
 
 // Widget define any TUI components in the widget tree.
 type Widget interface {
+	fmt.Stringer
 	render.Renderable
 
+	// Return the name of the widget.
 	Name() string
+	// Return the Unique Universal IDentifier of the widget.
 	UUID() uuid.UUID
 
+	// Parent return the parent layout.
 	Parent() Layout
 	adoptedBy(parent Layout)
 	// Position in parent children collection
 	slot() int
 
+	// Return the style of the widget
 	Style() *style.Node
-	String() string
 }
