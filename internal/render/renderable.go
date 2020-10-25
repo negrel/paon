8f9e338506508eb6481fbc8368335de7c4900f75
Alexandre Negrel <negrel.dev@protonmail.com>
2020-10-25 19:33:35 +0100

    updating render.renderable interface

--- internal/render/renderable.go
+++ internal/render/renderable.go
@@ -1,6 +1,11 @@
 package render
 
+import (
+	"github.com/negrel/paon/internal/utils"
+)
+
 // Renderable define any object that can produce a render Patch.
 type Renderable interface {
-	Render() Patch
+	// Render the object inside the given rectangle and return a Patch object.
+	Render(rectangle utils.Rectangle) Patch
 }
