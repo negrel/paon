1ffe5e0045e73f8d32de4ad85517c50c41aee9ba
Alexandre Negrel <negrel.dev@protonmail.com>
2020-10-25 21:44:28 +0100

    improving Text widget render function

--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.14
 
 require (
 	github.com/gdamore/tcell v1.3.0
-	github.com/golang/mock v1.4.4
 	github.com/google/uuid v1.1.1
+	github.com/mitchellh/go-wordwrap v1.0.1
 	github.com/negrel/debuggo v0.0.0-20201024091346-a616c45023ac
 	github.com/stretchr/testify v1.6.1
 )
--- pkg/tui/text.go
+++ pkg/tui/text.go
@@ -1,7 +1,13 @@
 package tui
 
 import (
+	"strings"
+
+	"github.com/gdamore/tcell"
+	"github.com/mitchellh/go-wordwrap"
+
 	"github.com/negrel/paon/internal/render"
+	"github.com/negrel/paon/internal/utils"
 	"github.com/negrel/paon/internal/widgets"
 )
 
@@ -20,6 +26,22 @@ func TextWidget(content string) *Text {
 	}
 }
 
-func (t Text) Render() render.Patch {
-	panic("implement me")
+// Render implements the widgets.Widget interface.
+func (t Text) Render(rect utils.Rectangle) (patch render.Patch) {
+	patch.Origin = rect.Min
+	patch.Frame = make([][]render.Cell, 1)
+
+	wrapped := wordwrap.WrapString(t.content, uint(rect.Width()))
+	for i, line := range strings.Split(wrapped, "\n") {
+		patch.Frame[i] = make([]render.Cell, len(line))
+
+		for j, r := range line {
+			patch.Frame[i][j] = render.Cell{
+				Style:   tcell.StyleDefault,
+				Content: r,
+			}
+		}
+	}
+
+	return patch
 }
