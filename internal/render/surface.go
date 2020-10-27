ad76261315767aa74deaffcbc988d9afa9573687
Alexandre Negrel <negrel.dev@protonmail.com>
2020-10-27 22:42:12 +0100

    improving render.Surface & tcell wrapper with events

--- internal/render/surface.go
+++ internal/render/surface.go
@@ -1,10 +1,18 @@
 package render
 
+import (
+	"github.com/negrel/paon/internal/events"
+	"github.com/negrel/paon/internal/utils"
+)
+
 // Surface define terminal window/surface to draw on.
 type Surface interface {
 	Update()
 	Apply(Patch)
-	Size() (w, h int)
+	Size() utils.Size
+	Width() int
+	Height() int
 	Clear()
 	Fini()
+	PollEvent() events.Event
 }
--- internal/render/tcell_screen.go
+++ internal/render/tcell_screen.go
@@ -3,12 +3,16 @@ package render
 import (
 	"github.com/gdamore/tcell"
 	"github.com/negrel/debuggo/pkg/log"
+
+	"github.com/negrel/paon/internal/events"
+	"github.com/negrel/paon/internal/utils"
 )
 
 var _ Surface = &tcellScreen{}
 
 type tcellScreen struct {
 	tcell.Screen
+	size utils.Size
 }
 
 // NewTcellSurface return a new Surface based on the github.com/gdamore/tcell screen.
@@ -25,8 +29,24 @@ func NewTcellSurface() (Surface, error) {
 	scr.EnableMouse()
 
 	return &tcellScreen{
-		scr,
+		Screen: scr,
+		size:   utils.Size{},
 	}, nil
+}
+
+// Size implements the Screen interface.
+func (t *tcellScreen) Size() utils.Size {
+	return t.size
+}
+
+// Width implements the Screen interface.
+func (t *tcellScreen) Width() int {
+	return t.size.Width()
+}
+
+// Height implements the Screen interface.
+func (t *tcellScreen) Height() int {
+	return t.size.Height()
 }
 
 // Update implements the Screen interface.
@@ -47,5 +67,34 @@ func (t *tcellScreen) Apply(patch Patch) {
 
 			t.SetContent(x, y, cell.Content, []rune{}, cell.Style)
 		}
+	}
+}
+
+// PollEvent implements the Screen interface.
+func (t *tcellScreen) PollEvent() events.Event {
+	ev := t.Screen.PollEvent()
+
+	switch event := ev.(type) {
+	case *tcell.EventResize:
+		oldSize := t.size
+
+		w, h := event.Size()
+		newSize := utils.Size{
+			X: w,
+			Y: h,
+		}
+		t.size = newSize
+
+		return events.MakeResizeEvent(newSize, oldSize)
+
+	case *tcell.EventMouse:
+		X, Y := event.Position()
+		return events.MakeClickEvent(utils.Point{X: X, Y: Y})
+
+	case *tcell.EventInterrupt:
+		return events.MakeInterruptEvent(ev.When().UnixNano())
+
+	default:
+		return events.UnsupportedEvent{}
 	}
 }
