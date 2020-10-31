7bbbe267a68bcd2d46275f89dbf610d9879c4d0c
Alexandre Negrel <negrel.dev@protonmail.com>
2020-10-31 22:04:09 +0100

    adding a Document node to pkg/tree

--- internal/tree/document.go
+++ internal/tree/document.go
@@ -1,21 +1,42 @@
 package tree
 
-var _ ContainerNode = Document{}
+import (
+	"github.com/google/uuid"
+	"github.com/negrel/debuggo/pkg/assert"
+	"github.com/negrel/debuggo/pkg/log"
+)
+
+var _ ParentNode = Document{}
 
 type Document struct {
-	*containerNode
+	*parentNode
+
+	elements map[uuid.UUID]Element
 }
 
 func NewDocument() *Document {
 	var doc *Document
 	doc = &Document{
-		containerNode: &containerNode{
+		parentNode: &parentNode{
 			Node: &node{
-				nType: DocumentNode,
+				nodeType: DocumentNodeType,
+				// Document own itself so when it adopt node
+				// it sets node owner to Document
 				owner: &doc,
 			},
 		},
+		elements: make(map[uuid.UUID]Element),
 	}
 
 	return doc
+}
+
+func (d *Document) registerElement(element Element) {
+	id := element.ID()
+	_, alreadyExist := d.elements[id]
+
+	assert.False(alreadyExist, "can't register element", element, ", element is already registered")
+	log.Infoln("registering element", element)
+
+	d.elements[id] = element
 }
