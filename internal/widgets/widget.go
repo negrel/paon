066f66589399d3f567af736db9623cecc1ca703b
Alexandre Negrel <negrel.dev@protonmail.com>
2020-11-08 11:00:45 +0100

    improving Widget in internal/widgets

--- internal/widgets/widget.go
+++ internal/widgets/widget.go
@@ -1,39 +1,73 @@
 package widgets
 
 import (
+	"github.com/negrel/paon/internal/events"
+	"github.com/negrel/paon/internal/geometry"
 	"github.com/negrel/paon/internal/render"
 	"github.com/negrel/paon/internal/tree"
 )
 
 type Widget interface {
 	tree.Node
+	events.EventTarget
 
-	Render(buffer render.Surface)
+	ParentL() Layout
+	NextW() Widget
+	PreviousW() Widget
+
+	layer() *render.Layer
+	Render(bounds geometry.Rectangle) render.Patch
 }
 
 var _ Widget = &widget{}
 
 type widget struct {
 	tree.Node
-
-	options []Option
+	events.Target
+	*render.Layer
 }
 
 func NewWidget(name string, opts ...Option) Widget {
-	return newWidget(name, opts...)
+	node := tree.NewNode(name)
+	return newWidget(node, opts...)
 }
 
-func newWidget(name string, opts ...Option) *widget {
+func newWidget(node tree.Node, opts ...Option) *widget {
 	w := &widget{
-		Node:    tree.NewNode(name),
-		options: opts,
+		Node:  node,
+		Layer: render.NewLayer(node),
+	}
+
+	for _, opt := range opts {
+		opt(w)
 	}
 
 	return w
 }
 
-func (w *widget) Render(buffer render.Surface) {
-	for _, opt := range w.options {
-		opt.apply(buffer)
+func (w *widget) ParentL() Layout {
+	if p := w.Parent(); p != nil {
+		return w.Parent().(Layout)
 	}
+	return nil
+}
+
+func (w *widget) NextW() Widget {
+	if n := w.Next(); n != nil {
+		return n.(Widget)
+	}
+
+	return nil
+}
+
+func (w *widget) PreviousW() Widget {
+	if p := w.Previous(); p != nil {
+		return p.(Widget)
+	}
+
+	return nil
+}
+
+func (w *widget) layer() *render.Layer {
+	return w.Layer
 }
