f5ae8dcb2cb486a0ef1a719817178f68727bfc34
Alexandre Negrel <negrel.dev@protonmail.com>
2021-01-02 18:59:30 +0100

    adding new widgets option

--- pkg/pdk/widgets/option.go
+++ pkg/pdk/widgets/option.go
@@ -2,13 +2,34 @@ package widgets
 
 import (
 	"github.com/negrel/paon/internal/events"
+	pdkstyle "github.com/negrel/paon/pkg/pdk/style"
+	"github.com/negrel/paon/pkg/pdk/style/property"
 )
 
-type Option func(widget Widget)
-type LayoutOption func(layout Layout)
+type Option func(widget *widget)
+
+func ID(id string) Option {
+	return func(widget *widget) {
+		widget.id = id
+	}
+}
 
 func Listener(eventType events.EventType, listener events.Listener) Option {
-	return func(widget Widget) {
+	return func(widget *widget) {
 		widget.AddEventListener(eventType, &listener)
+	}
+}
+
+func Themes(themes ...pdkstyle.Theme) Option {
+	return func(widget *widget) {
+		widget.theme.themes = themes
+	}
+}
+
+func Props(props ...property.Property) Option {
+	return func(widget *widget) {
+		for _, prop := range props {
+			widget.theme.Set(prop)
+		}
 	}
 }
