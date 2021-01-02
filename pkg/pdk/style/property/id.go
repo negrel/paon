a1c28bd94f2d191a96abed6938b1c7354c72e8e7
Alexandre Negrel <negrel.dev@protonmail.com>
2021-01-02 19:58:16 +0100

    auto generated property id for custom props (to avoid collision)

--- pkg/pdk/style/property/id.go
+++ pkg/pdk/style/property/id.go
@@ -1,14 +1,19 @@
 package property
 
-import "fmt"
+import (
+	"fmt"
+	"sync/atomic"
+)
 
 //go:generate stringer -type=ID -trimprefix=ID
 var _ fmt.Stringer = ID(0)
 
-type ID int
+type ID int32
 
 const (
-	IDWidth ID = iota + 1
+	IDDisplay ID = iota + 1
+
+	IDWidth
 	IDMinWidth
 	IDMaxWidth
 
@@ -27,4 +32,13 @@ const (
 	IDPaddingBottom
 
 	IDBackgroundColor
+
+	// Used for custom property created using the pdk
+	unusedID
 )
+
+var propIdCounter int32 = int32(unusedID - 1)
+
+func MakeID() ID {
+	return ID(atomic.AddInt32(&propIdCounter, 1))
+}
--- pkg/pdk/style/property/id_string.go
+++ pkg/pdk/style/property/id_string.go
@@ -8,25 +8,28 @@ func _() {
 	// An "invalid array index" compiler error signifies that the constant values have changed.
 	// Re-run the stringer command to generate them again.
 	var x [1]struct{}
-	_ = x[IDWidth-1]
-	_ = x[IDMinWidth-2]
-	_ = x[IDMaxWidth-3]
-	_ = x[IDHeight-4]
-	_ = x[IDMinHeight-5]
-	_ = x[IDMaxHeight-6]
-	_ = x[IDMarginLeft-7]
-	_ = x[IDMarginTop-8]
-	_ = x[IDMarginRight-9]
-	_ = x[IDMarginBottom-10]
-	_ = x[IDPaddingLeft-11]
-	_ = x[IDPaddingTop-12]
-	_ = x[IDPaddingRight-13]
-	_ = x[IDPaddingBottom-14]
+	_ = x[IDDisplay-1]
+	_ = x[IDWidth-2]
+	_ = x[IDMinWidth-3]
+	_ = x[IDMaxWidth-4]
+	_ = x[IDHeight-5]
+	_ = x[IDMinHeight-6]
+	_ = x[IDMaxHeight-7]
+	_ = x[IDMarginLeft-8]
+	_ = x[IDMarginTop-9]
+	_ = x[IDMarginRight-10]
+	_ = x[IDMarginBottom-11]
+	_ = x[IDPaddingLeft-12]
+	_ = x[IDPaddingTop-13]
+	_ = x[IDPaddingRight-14]
+	_ = x[IDPaddingBottom-15]
+	_ = x[IDBackgroundColor-16]
+	_ = x[unusedID-17]
 }
 
-const _ID_name = "WidthMinWidthMaxWidthHeightMinHeightMaxHeightMarginLeftMarginTopMarginRightMarginBottomPaddingLeftPaddingTopPaddingRightPaddingBottom"
+const _ID_name = "DisplayWidthMinWidthMaxWidthHeightMinHeightMaxHeightMarginLeftMarginTopMarginRightMarginBottomPaddingLeftPaddingTopPaddingRightPaddingBottomBackgroundColorunusedID"
 
-var _ID_index = [...]uint8{0, 5, 13, 21, 27, 36, 45, 55, 64, 75, 87, 98, 108, 120, 133}
+var _ID_index = [...]uint8{0, 7, 12, 20, 28, 34, 43, 52, 62, 71, 82, 94, 105, 115, 127, 140, 155, 163}
 
 func (i ID) String() string {
 	i -= 1
