c3703c8b2f81ec20a66f03a711cbc0936db16824
Alexandre Negrel <negrel.dev@protonmail.com>
2021-01-03 20:33:07 +0100

    adding Bind option

--- pkg/pdk/widgets/option.go
+++ pkg/pdk/widgets/option.go
@@ -8,25 +8,38 @@ import (
 
 type Option func(widget *widget)
 
+// Bind the given variable to the widget.
+func Bind(variable *Widget) Option {
+	return func(widget *widget) {
+		*variable = widget
+	}
+}
+
+// ID set the widget id (must be unique across your entire application).
 func ID(id string) Option {
 	return func(widget *widget) {
 		widget.id = id
 	}
 }
 
+// Listener add the given events.Listener for the given events.EventType to your widget.
 func Listener(eventType events.EventType, listener events.Listener) Option {
 	return func(widget *widget) {
 		widget.AddEventListener(eventType, &listener)
 	}
 }
 
+// Themes apply the given themes to the widget. Those theme can be shared across multiple
+// widget and won't be modified by yourWidget.Theme().Set(property) method.
 func Themes(themes ...pdkstyle.Theme) Option {
 	return func(widget *widget) {
 		widget.theme.themes = themes
 	}
 }
 
-func Props(props ...property.Property) Option {
+// Theme apply the given property to the widget theme. This theme is not shareable
+// and can only be modified using the yourWidget.Theme().Set(property) method.
+func Theme(props ...property.Property) Option {
 	return func(widget *widget) {
 		for _, prop := range props {
 			widget.theme.Set(prop)
