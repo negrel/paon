43ca25f83af4d3add44970d52e57e216a56154eb
Alexandre Negrel <negrel.dev@protonmail.com>
2021-01-05 20:39:56 +0100

    updating events import in internal/draw pkg

--- internal/draw/window.go
+++ internal/draw/window.go
@@ -1,8 +1,8 @@
 package draw
 
 import (
-	"github.com/negrel/paon/internal/events"
 	"github.com/negrel/paon/internal/geometry"
+	"github.com/negrel/paon/pkg/pdk/events"
 )
 
 // Window define a terminal window to apply render Canvas on.
