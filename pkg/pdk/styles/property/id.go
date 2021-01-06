c9fbbfc76ecd75311b9a03a95b4948d545e539c6
Alexandre Negrel <negrel.dev@protonmail.com>
2021-01-06 19:23:37 +0100

    property.ID is now thread-safe and implements fmt.Stringer interface

--- pkg/pdk/styles/property/id.go
+++ pkg/pdk/styles/property/id.go
@@ -2,10 +2,10 @@ package property
 
 import (
 	"fmt"
+	"sync"
 	"sync/atomic"
 )
 
-//go:generate stringer -type=ID -trimprefix=ID
 var _ fmt.Stringer = ID(0)
 
 type ID int32
@@ -37,8 +37,57 @@ const (
 	unusedID
 )
 
+func (id ID) name() string {
+	return idName.get(id)
+}
+
+func (id ID) String() string {
+	return fmt.Sprintf("%v (%d)", id.name(), id)
+}
+
+type idMap struct {
+	sync.Mutex
+	m map[ID]string
+}
+
+func (im *idMap) set(i ID, name string) {
+	im.Lock()
+	defer im.Unlock()
+	im.m[i] = name
+}
+
+func (im *idMap) get(i ID) string {
+	im.Lock()
+	defer im.Unlock()
+	return im.m[i]
+}
+
+var idName = &idMap{
+	Mutex: sync.Mutex{},
+	m: map[ID]string{
+		IDDisplay:         "display",
+		IDWidth:           "width",
+		IDMinWidth:        "min-width",
+		IDMaxWidth:        "max-width",
+		IDHeight:          "height",
+		IDMinHeight:       "min-height",
+		IDMaxHeight:       "max-height",
+		IDMarginLeft:      "margin-left",
+		IDMarginTop:       "margin-top",
+		IDMarginRight:     "margin-right",
+		IDMarginBottom:    "margin-bottom",
+		IDPaddingLeft:     "padding-left",
+		IDPaddingTop:      "padding-top",
+		IDPaddingRight:    "padding-right",
+		IDPaddingBottom:   "padding-bottom",
+		IDBackgroundColor: "background-color",
+	},
+}
+
 var propIdCounter int32 = int32(unusedID - 1)
 
-func MakeID() ID {
-	return ID(atomic.AddInt32(&propIdCounter, 1))
+func MakeID(name string) ID {
+	id := ID(atomic.AddInt32(&propIdCounter, 1))
+	idName.set(id, name)
+	return id
 }
