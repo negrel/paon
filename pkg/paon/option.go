89f37c992cfdbeabf4a5924482adfdac03352170
Alexandre Negrel <negrel.dev@protonmail.com>
2021-01-20 15:15:34 +0100

    upgrading dependencies

--- pkg/paon/option.go
+++ pkg/paon/option.go
@@ -7,12 +7,15 @@ import (
 
 type Option func(*App)
 
+// Clock is an option to set the internal clock used in the App for
+// rendering.
 func Clock(ticker *time.Ticker) Option {
 	return func(app *App) {
 		app.clock = ticker
 	}
 }
 
+// Screen is an option to use a different screen backend (default is tcell)
 func Screen(screen draw.Screen) Option {
 	return func(app *App) {
 		app.window.Screen = screen
