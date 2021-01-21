0f51942486fbb8f5067ae4608aa4baad2705cdbe
Alexandre Negrel <negrel.dev@protonmail.com>
2021-01-21 14:13:43 +0100

    adding resize listener constructor

--- pkg/pdk/events/resize.go
+++ pkg/pdk/events/resize.go
@@ -1,10 +1,30 @@
 package events
 
 import (
+	"github.com/negrel/debuggo/pkg/log"
 	"github.com/negrel/paon/internal/geometry"
 )
 
 var _ Event = Resize{}
+
+// ResizeListener convert the given event handler as a generic Listener.
+func ResizeListener(handler func(Resize)) *Listener {
+	l := Listener{
+		Type: TypeMouseMove,
+		Handle: func(event Event) {
+			re, ok := event.(Resize)
+
+			if !ok {
+				log.Warnf("\"%v\" listener expected, but got %v", TypeMouseMove, event.Type())
+				return
+			}
+
+			handler(re)
+		},
+	}
+
+	return &l
+}
 
 // Resize is triggered when the user resize rendering surface.
 type Resize struct {
