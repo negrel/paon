e53bd80b88d5d5e890eb94e52879b2a5d1e866e5
Alexandre Negrel <negrel.dev@protonmail.com>
2021-02-13 09:56:41 +0100

    replace draw.Context render.Buffer by draw.Canvas

--- internal/draw/context.go
+++ internal/draw/context.go
@@ -1,17 +1,8 @@
 package draw
 
-import "github.com/negrel/paon/internal/render"
-
 // Context is the rendering context passed to Object for the layout step and the draw step.
 type Context struct {
 	Object     Object
-	Canvas     *render.Buffer
+	Canvas     Canvas
 	Constraint Constraint
-}
-
-func MakeContext(constraint Constraint) Context {
-	return Context{
-		Canvas:     render.NewCanvas(constraint.Max),
-		Constraint: constraint,
-	}
 }
