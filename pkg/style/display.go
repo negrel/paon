4d845b3aee1f2c9e8d510ce2cdbd5cb3bb213ec3
Alexandre Negrel <negrel.dev@protonmail.com>
2021-02-16 19:42:47 +0100

    updating display prop constructor

--- pkg/style/display.go
+++ pkg/style/display.go
@@ -2,18 +2,18 @@ package style
 
 import (
 	"github.com/negrel/debuggo/pkg/assert"
-	"github.com/negrel/paon/pkg/pdk/painter"
+	"github.com/negrel/paon/pkg/pdk/layout"
 	"github.com/negrel/paon/pkg/pdk/styles/property"
 )
 
 const (
-	DisplayHidden = painter.Hidden
-	DisplayBlock  = painter.Block
-	DisplayFlex   = painter.Flex
+	DisplayHidden = layout.Hidden
+	DisplayBlock  = layout.Block
+	DisplayFlex   = layout.Flex
 )
 
 func Display(value int) property.Int {
-	assert.True(painter.IsValidRendererID(value))
+	assert.True(layout.IsValidAlgorithmID(value))
 
 	return property.MakeInt(property.IDDisplay, value)
 }
