5381b30c2f68ff5cd496874dc81bda82028e0815
Alexandre Negrel <negrel.dev@protonmail.com>
2021-02-16 20:31:54 +0100

    adding draw.Cell to the render package

--- pkg/pdk/render/cell.go
+++ pkg/pdk/render/cell.go
@@ -1,7 +1,6 @@
 package render
 
 import (
-	"github.com/gdamore/tcell/v2"
 	"github.com/negrel/paon/pkg/pdk/styles/value"
 )
 
@@ -11,19 +10,6 @@ type CellStyle struct {
 
 	Bold, Blink, Reverse, Underline,
 	Dim, Italic, StrikeThrough bool
-}
-
-func (cs CellStyle) toTcellStyle() tcell.Style {
-	return tcell.StyleDefault.
-		Foreground(tcell.Color(cs.Foreground.Int32())).
-		Background(tcell.Color(cs.Background.Int32())).
-		Bold(cs.Bold).
-		Blink(cs.Blink).
-		Reverse(cs.Reverse).
-		Underline(cs.Underline).
-		Dim(cs.Dim).
-		Italic(cs.Italic).
-		StrikeThrough(cs.StrikeThrough)
 }
 
 // Cell define a terminal Surface cell.
