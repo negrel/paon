dda99b232e83ffb3380ce3e5657754c36ff7a33c
Alexandre Negrel <negrel.dev@protonmail.com>
2021-02-16 20:32:18 +0100

    adding an helper method to iter over the cells of a render.Patch

--- pkg/pdk/render/patch.go
+++ pkg/pdk/render/patch.go
@@ -9,3 +9,16 @@ type Patch struct {
 	Pos   geometry.Point
 	Cells [][]*Cell
 }
+
+func (p Patch) ForEachCell(fn func(pos geometry.Point, cell *Cell)) {
+	height := len(p.Cells)
+	width := len(p.Cells[0])
+
+	for i := 0; i < height; i++ {
+		y := p.Pos.Y() + i
+		for j := 0; j < width; j++ {
+			x := p.Pos.X() + j
+			fn(geometry.Pt(x, y), p.Cells[i][j])
+		}
+	}
+}
