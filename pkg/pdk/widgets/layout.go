658a5def2730aeaa01f7a01b23e4773d11085f1d
Alexandre Negrel <negrel.dev@protonmail.com>
2021-03-09 23:18:32 +0100

    improving widgets.Layout interface

--- pkg/pdk/widgets/layout.go
+++ pkg/pdk/widgets/layout.go
@@ -2,6 +2,7 @@ package widgets
 
 import (
 	"github.com/negrel/paon/internal/tree"
+	"github.com/negrel/paon/pkg/pdk/id"
 )
 
 // Layout is a Widget that can contain child Widget.
@@ -18,14 +19,14 @@ type Layout interface {
 	// An error is returned if the given Widget is an ancestor
 	// of this Layout.
 	// This function panic if a nil value is given.
-	AppendChild(child Widget) error
+	AppendChild(child Widget) (Widget, error)
 
 	// Insert the given Widget before the reference Widget in the children list.
 	// If the reference is nil the child is appended.
 	// An error is returned if the given Widget is an ancestor
 	// of this Layout.
 	// This function panic if a nil child value is given.
-	InsertBefore(reference, child Widget) error
+	InsertBefore(reference, child Widget) (Widget, error)
 
 	// Remove the given Widget of the children list of this Layout.
 	// An error is returned if the Widget is not a direct child of
@@ -59,6 +60,11 @@ func newLayout(node tree.ParentNode) *layout {
 	}
 }
 
+// ID implements the id.Identifiable interface.
+func (l *layout) ID() id.ID {
+	return l.widget.ID()
+}
+
 // LastChild implements the Layout interface.
 func (l *layout) FirstChild() Widget {
 	if fc := l.FirstChildNode(); fc != nil {
@@ -78,16 +84,64 @@ func (l *layout) LastChild() Widget {
 }
 
 // AppendChild implements the Layout interface.
-func (l *layout) AppendChild(child Widget) error {
-	return l.AppendChildNode(child)
+func (l *layout) AppendChild(child Widget) (Widget, error) {
+	err := l.AppendChildNode(child)
+
+	return child, err
 }
 
 // InsertBefore implements the Layout interface.
-func (l *layout) InsertBefore(reference, child Widget) error {
-	return l.InsertBeforeNode(reference, child)
+func (l *layout) InsertBefore(reference, child Widget) (Widget, error) {
+	err := l.InsertBeforeNode(reference, child)
+
+	return child, err
 }
 
 // RemoveChild implements the Layout interface.
 func (l *layout) RemoveChild(child Widget) error {
 	return l.RemoveChildNode(child)
 }
+
+//var _ Widget = &childWidget{}
+//
+//type childWidget struct {
+//	Widget
+//	box        *flows.Box
+//	constraint flows.Constraint
+//}
+//
+//func wrapWidget(widget Widget) *childWidget {
+//	cw, ok := widget.(*childWidget)
+//	if ok {
+//		return cw
+//	}
+//
+//	return &childWidget{
+//		Widget:     widget,
+//		box:        nil,
+//		constraint: flows.Constraint{},
+//	}
+//}
+//
+//// flow implements the Widget interface.
+//func (cw *childWidget) flow(constraint flows.Constraint) *flows.Box {
+//	if cw.constraint.Equals(constraint) && cw.box != nil {
+//		return cw.box
+//	}
+//
+//	cw.constraint = constraint
+//	box := cw.Widget.flow(constraint)
+//	cw.box = box
+//
+//	assert.True(box.Width() > constraint.Min.Width())
+//	assert.True(box.Width() < constraint.Max.Width())
+//	assert.True(box.Height() > constraint.Min.Height())
+//	assert.True(box.Height() < constraint.Max.Height())
+//
+//	return box
+//}
+//
+//// Box implements the Widget interface.
+//func (cw *childWidget) Box() flows.BoxModel {
+//	return cw.box
+//}
