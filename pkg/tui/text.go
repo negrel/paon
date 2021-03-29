c724bdcc31d56986bdfe2db2d63ce4c70ac068c0
Alexandre Negrel <negrel.dev@protonmail.com>
2021-03-29 21:15:22 +0200

    text widget can be multiline now

--- pkg/tui/text.go
+++ pkg/tui/text.go
@@ -1,6 +1,7 @@
 package tui
 
 import (
+	"github.com/mitchellh/go-wordwrap"
 	"github.com/negrel/paon/internal/geometry"
 	"github.com/negrel/paon/pkg/pdk/draw"
 	"github.com/negrel/paon/pkg/pdk/flows"
@@ -10,6 +11,7 @@ import (
 	"github.com/negrel/paon/pkg/pdk/styles/value"
 	"github.com/negrel/paon/pkg/pdk/widgets"
 	"github.com/negrel/paon/pkg/style"
+	"strings"
 )
 
 var _ widgets.Widget = &TextWidget{}
@@ -17,7 +19,8 @@ var _ widgets.Widget = &TextWidget{}
 type TextWidget struct {
 	widgets.Widget
 
-	content string
+	content    string
+	fmtContent []string
 }
 
 // Text returns a new TextWidget with the given content.
@@ -30,6 +33,7 @@ func Text(content string, options ...widgets.Option) *TextWidget {
 	defaultStyle.Set(style.FgColor(value.ColorFromHex(0xFFFFFF)))
 
 	tw.Widget = widgets.NewWidget(
+		"text",
 		widgets.PrependOptions(
 			options,
 			widgets.Algo(tw.flow),
@@ -41,10 +45,18 @@ func Text(content string, options ...widgets.Option) *TextWidget {
 	return tw
 }
 
+func (tw *TextWidget) flowText(width uint) int {
+	tw.fmtContent = strings.Split(wordwrap.WrapString(tw.content, width), "\n")
+
+	return len(tw.fmtContent)
+}
+
 func (tw *TextWidget) flow(constraint flows.Constraint) flows.BoxModel {
 	return flows.Block(tw.Style(), constraint, func(constraint flows.Constraint) flows.BoxModel {
+		height := tw.flowText(uint(constraint.Max.Width()))
+
 		width := math.Constrain(len(tw.content), constraint.Min.Width(), constraint.Max.Width())
-		height := math.Constrain(1, constraint.Min.Height(), constraint.Max.Height())
+		height = math.Constrain(height, constraint.Min.Height(), constraint.Max.Height())
 
 		box := flows.NewBox(geometry.Rectangle{
 			Min: constraint.Min.Min,
@@ -64,6 +76,9 @@ func (tw *TextWidget) draw(ctx draw.Context) {
 		ctx.SetFillColor(color.Color)
 	}
 
-	ctx.FillTextH(geometry.Pt(0, 0), tw.content)
+	for i, line := range tw.fmtContent {
+		ctx.FillTextH(geometry.Pt(0, i), line)
+	}
+
 	ctx.Commit()
 }
