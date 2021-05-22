f1efe42ff15b06b24cf6c40d933aff53aacee3f0
Alexandre Negrel <negrel.dev@protonmail.com>
2021-05-22 16:05:34 +0200

    add minimal doc to tree package and subpackage

---
+++ pdk/tree/doc.go
@@ -0,0 +1,2 @@
+// Package tree provides a generic Node interface to build tree data structures.
+package tree
---
+++ pdk/tree/events/doc.go
@@ -0,0 +1,3 @@
+// Package tree/events provide a wrapper around tree.Node objects to improve performance
+// and add LifeCycle events to the nodes.
+package events
