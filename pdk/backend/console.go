081329dfd32484e14d2012ca801e9bf1fa2a5fe9
Alexandre Negrel <negrel.dev@protonmail.com>
2021-05-22 20:06:11 +0200

    add a backend package with generic interface for backends

---
+++ pdk/backend/console.go
@@ -0,0 +1,23 @@
+package backend
+
+import (
+	"github.com/negrel/paon/pdk/draw"
+	"github.com/negrel/paon/pdk/events"
+)
+
+// Console define a generic console used for rendering and event loops.
+type Console interface {
+	draw.Canvas
+
+	// Clear clears the entire console surface and replace cell by empty black one.
+	Clear()
+
+	// Flush flushes the cell buffer.
+	Flush()
+
+	// Start initializes the console for use. This starts the event loop and rendering.
+	Start(chan<- events.Event) error
+
+	// Stop deinitializes the console.
+	Stop()
+}
