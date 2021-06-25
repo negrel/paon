481c255ef05f620c6596898db5bb6b1288206d7e
Alexandre Negrel <negrel.dev@protonmail.com>
2021-06-25 22:44:37 +0200

    feat: add support for default colors (tcell backend)

--- pdk/backend/tcell/cell.go
+++ pdk/backend/tcell/cell.go
@@ -6,37 +6,53 @@ import (
 	"github.com/negrel/paon/pdk/styles/value"
 )
 
+var defaultFg, defaultBg, defaultAttr = tcell.StyleDefault.Decompose()
+
 func fromTcell(mainc rune, combc []rune, style tcell.Style, width int) draw.Cell {
 	fg, bg, attrs := style.Decompose()
 
-	return draw.Cell{
+	cell := draw.Cell{
 		Style: draw.CellStyle{
-			Foreground: value.ColorFromHex(fg.Hex()),
-			Background: value.ColorFromHex(bg.Hex()),
-			Bold:       (attrs & tcell.AttrBold) != 0,
-			Blink:      (attrs & tcell.AttrBlink) != 0,
-			Reverse:    (attrs & tcell.AttrReverse) != 0,
-			Underline:  (attrs & tcell.AttrUnderline) != 0,
-			Dim:        (attrs & tcell.AttrDim) != 0,
-			Italic:     (attrs & tcell.AttrItalic) != 0,
+			Bold:      (attrs & tcell.AttrBold) != 0,
+			Blink:     (attrs & tcell.AttrBlink) != 0,
+			Reverse:   (attrs & tcell.AttrReverse) != 0,
+			Underline: (attrs & tcell.AttrUnderline) != 0,
+			Dim:       (attrs & tcell.AttrDim) != 0,
+			Italic:    (attrs & tcell.AttrItalic) != 0,
 		},
 		Content: mainc,
 	}
+
+	if fg != defaultFg {
+		cell.Style.Foreground = value.ColorFromHex(fg.Hex())
+	}
+	if bg != defaultBg {
+		cell.Style.Background = value.ColorFromHex(bg.Hex())
+	}
+
+	return cell
 }
 
 func toTcell(cell draw.Cell) (mainc rune, combc []rune, style tcell.Style) {
-	return cell.Content, []rune{},
-		tcell.StyleDefault.
-			Foreground(
-				tcell.NewHexColor(cell.Style.Foreground.Int32()),
-			).
-			Background(
-				tcell.NewHexColor(cell.Style.Background.Int32()),
-			).
-			Bold(cell.Style.Bold).
-			Blink(cell.Style.Blink).
-			Reverse(cell.Style.Reverse).
-			Underline(cell.Style.Underline).
-			Dim(cell.Style.Dim).
-			Italic(cell.Style.Italic)
+	style = tcell.StyleDefault.
+		Bold(cell.Style.Bold).
+		Blink(cell.Style.Blink).
+		Reverse(cell.Style.Reverse).
+		Underline(cell.Style.Underline).
+		Dim(cell.Style.Dim).
+		Italic(cell.Style.Italic)
+
+	if cell.Style.Foreground.IsSet() {
+		style = style.Foreground(
+			tcell.NewHexColor(cell.Style.Foreground.Int32()),
+		)
+	}
+
+	if cell.Style.Background.IsSet() {
+		style = style.Background(
+			tcell.NewHexColor(cell.Style.Background.Int32()),
+		)
+	}
+
+	return cell.Content, []rune{}, style
 }
--- pdk/styles/value/color.go
+++ pdk/styles/value/color.go
@@ -1,29 +1,41 @@
 package value
 
+import "github.com/negrel/debuggo/code_gen/log/log"
+
 // Color define a 24-bit RGB color.
 type Color struct {
 	R, G, B uint8
+	isSet   bool
 }
 
 // ColorFromRGB returns a Color object using the given color channel value.
 func ColorFromRGB(r, g, b uint8) Color {
 	return Color{
-		R: r,
-		G: g,
-		B: b,
+		R:     r,
+		G:     g,
+		B:     b,
+		isSet: true,
 	}
 }
 
 // ColorFromHex returns a Color object using the given hexadecimal color code.
 func ColorFromHex(rgb int32) Color {
 	return Color{
-		R: uint8((rgb & 0xFF0000) >> 16),
-		G: uint8((rgb & 0xFF00) >> 8),
-		B: uint8(rgb & 0xFF),
+		R:     uint8((rgb & 0xFF0000) >> 16),
+		G:     uint8((rgb & 0xFF00) >> 8),
+		B:     uint8(rgb & 0xFF),
+		isSet: true,
 	}
 }
 
 // Int32 converts this Color to an int32.
 func (c Color) Int32() int32 {
 	return int32(c.R)<<16 | int32(c.G)<<8 | int32(c.B)
+}
+
+// IsSet returns true if the color is set (to make distinction between black and unset colors).
+func (c Color) IsSet() bool {
+	log.Debug(c.isSet)
+	log.Debug(c.Int32())
+	return c.isSet && c.Int32() != 0
 }
