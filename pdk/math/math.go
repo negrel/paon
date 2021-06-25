3b333c6ba633af1ea0364361ae0e8d0f3cc05d37
Alexandre Negrel <negrel.dev@protonmail.com>
2021-06-25 21:20:31 +0200

    feat: add math package

---
+++ pdk/math/math.go
@@ -0,0 +1,39 @@
+package math
+
+import "github.com/negrel/debuggo/pkg/assert"
+
+// Constrain the value between the given minimum and maximum.
+func Constrain(value, min, max int) int {
+	value = Max(value, min)
+	value = Min(value, max)
+
+	return value
+}
+
+// Min returns the smallest integer.
+func Min(nbrs ...int) int {
+	assert.Greater(len(nbrs), 0)
+
+	min := nbrs[0]
+	for _, nbr := range nbrs {
+		if nbr < min {
+			min = nbr
+		}
+	}
+
+	return min
+}
+
+// Max returns the biggest integer.
+func Max(nbrs ...int) int {
+	assert.Greater(len(nbrs), 0)
+
+	max := nbrs[0]
+	for _, nbr := range nbrs {
+		if nbr > max {
+			max = nbr
+		}
+	}
+
+	return max
+}
