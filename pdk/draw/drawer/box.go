e522cbdb54f90276fe76e522fa2ada63f5d2addb
Alexandre Negrel <negrel.dev@protonmail.com>
2021-07-14 18:58:20 +0200

    refacto pdk/draw

---
+++ pdk/draw/drawer/box.go
@@ -0,0 +1,23 @@
+package drawer
+
+import (
+	"github.com/negrel/paon/pdk/draw"
+	"github.com/negrel/paon/pdk/layout"
+	"github.com/negrel/paon/styles"
+	"github.com/negrel/paon/styles/property"
+)
+
+// Box draws a given BoxModel with the given styles.Style on the given draw.Context.
+func Box(styled styles.Styled, boxed layout.Boxed) draw.DrawerFn {
+	return func(ctx draw.Context) {
+		style := styled.Style()
+		box := boxed.Box()
+
+		// Background color
+		bgColor, ok := style.Get(property.BackgroundColorID()).(property.Color)
+		if ok {
+			ctx.SetFillColor(bgColor.Color)
+		}
+		ctx.FillRectangle(box.PaddingBox())
+	}
+}
