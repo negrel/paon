5da398b9075bfeb0911ebbdc04c12308fd45b32d
Alexandre Negrel <negrel.dev@protonmail.com>
2021-08-02 22:55:10 +0200

    refacto: draw.Context is now a struct

--- pdk/draw/canvas.go
+++ pdk/draw/canvas.go
@@ -1,6 +1,8 @@
 package draw
 
 import (
+	stdcontext "context"
+
 	"github.com/negrel/paon/internal/geometry"
 )
 
@@ -17,12 +19,6 @@ type Canvas interface {
 	// Set sets the cell at the given position.
 	Set(geometry.Point, Cell)
 
-	// NewContext creates a new Context that can on this Canvas within the given bounds.
-	NewContext(bounds geometry.Rectangle) Context
-}
-
-// SubContext returns a new Context bounded within the given Context and rectangle.
-// The new subcontext use the same underlying Canvas as the given Context.
-func SubContext(ctx Context, bounds geometry.Rectangle) Context {
-	return ctx.Canvas().NewContext(ctx.Bounds().Mask(bounds))
+	// NewContext creates a new Context that can draw on this Canvas within the given bounds.
+	NewContext(ctx stdcontext.Context, bounds geometry.Rectangle) *Context
 }
