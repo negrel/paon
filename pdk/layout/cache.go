acf25676a35dc63d137f3dadbd979d96e4c58d90
Alexandre Negrel <negrel.dev@protonmail.com>
2021-08-02 23:03:39 +0200

    feat: add a valid field to layout cache

--- pdk/layout/cache.go
+++ pdk/layout/cache.go
@@ -1,6 +1,11 @@
 package layout
 
-import "github.com/negrel/debuggo/pkg/assert"
+import (
+	"github.com/negrel/debuggo/pkg/assert"
+	"github.com/negrel/paon/internal/geometry"
+)
+
+var unsetSize = geometry.NewSize(-1, -1)
 
 var _ Boxed = &Cache{}
 var _ Manager = &Cache{}
@@ -8,6 +13,7 @@ var _ Manager = &Cache{}
 // Cache is a wrapper for Flowable object.
 type Cache struct {
 	Manager
+	valid      bool
 	cache      BoxModel
 	constraint Constraint
 }
@@ -27,7 +33,7 @@ func (c *Cache) Layout(constraint Constraint) BoxModel {
 	// the cache is still valid if the new constraint has the same size
 	// than the cached constraint and the distance between the Min and Max
 	// rectangle remains the same.
-	if c.cache != nil && c.constraint.EqualsSize(constraint) &&
+	if c.valid && c.constraint.EqualsSize(constraint) &&
 		c.constraint.Min.Min.Sub(c.constraint.Max.Min) == constraint.Min.Min.Sub(constraint.Max.Min) {
 		box := c.cache
 
@@ -44,18 +50,19 @@ func (c *Cache) Layout(constraint Constraint) BoxModel {
 	// Update cache
 	c.constraint = constraint
 	c.cache = c.Manager.Layout(constraint)
+	c.valid = true
 
 	return c.cache
 }
 
-// Invalidate invalidates the cache data.
-func (c *Cache) Invalidate() {
-	c.cache = nil
-}
-
 // IsValid returns true if the cache data is valid.
 func (c *Cache) IsValid() bool {
-	return c.cache != nil
+	return c.valid
+}
+
+// Invalidate marks the cache as invalid.
+func (c *Cache) Invalidate() {
+	c.valid = false
 }
 
 // Constraint returns the cached constraint of the last layout.
