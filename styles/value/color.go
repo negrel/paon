f7ff96234a83acc0b04d1e91894f78e75fc2b22d
Alexandre Negrel <negrel.dev@protonmail.com>
2021-09-12 11:53:27 +0200

    rewriting color type to support RGBA

--- styles/value/color.go
+++ styles/value/color.go
@@ -1,43 +1,63 @@
 package value
 
-// Color define a 24-bit RGB color.
-type Color struct {
-	R, G, B uint8
-	isSet   bool
-}
+// Color define a 32-bit RGBA color.
+// Color are stored as uint32 of the following format: 0xAARRGGBB
+type Color uint32
 
 // ColorFromRGB returns a Color object using the given color channels values.
 func ColorFromRGB(r, g, b uint8) Color {
-	return Color{
-		R:     r,
-		G:     g,
-		B:     b,
-		isSet: true,
-	}
+	return ColorFromRGBA(r, g, b, 255)
+}
+
+// ColorFromRGBA returns a Color object using the given color channels values.
+func ColorFromRGBA(r, g, b, a uint8) Color {
+	return Color(uint32(a)<<24 | uint32(r)<<16 | uint32(g)<<8 | uint32(b))
+}
+
+// ColorFromHexa returns a Color object using the given hexadecimal color code.
+// The color is interpreted as OxAARRGGBB
+func ColorFromHexa(rgba uint32) Color {
+	return Color(rgba)
 }
 
 // ColorFromHex returns a Color object using the given hexadecimal color code.
-func ColorFromHex(rgb int32) Color {
-	return Color{
-		R:     uint8((rgb & 0xFF0000) >> 16),
-		G:     uint8((rgb & 0xFF00) >> 8),
-		B:     uint8(rgb & 0xFF),
-		isSet: true,
-	}
+// The color is interpreted as OxRRGGBB
+func ColorFromHex(rgb uint32) Color {
+	return ColorFromHexa((rgb | 0xFF000000))
 }
 
 // ColorUnset returns an unset Color.
 // Editing the RGB channels of an unset color is useless.
 func ColorUnset() Color {
-	return Color{}
+	return Color(0)
 }
 
-// Hex converts this Color to an int32.
-func (c Color) Hex() int32 {
-	return int32(c.R)<<16 | int32(c.G)<<8 | int32(c.B)
+// Hex converts this Color to an uint32. The color is interpreted as 0xRRGGBB
+func (c Color) Hex() uint32 {
+	return uint32(c) & 0xFFFFFF
 }
 
-// IsSet returns true if the color is set (to make distinction between black and unset colors).
-func (c Color) IsSet() bool {
-	return c.isSet
+// Hexa converts this Color to an uint32. The color is interpreted as 0xAARRGGBB
+func (c Color) Hexa() uint32 {
+	return uint32(c)
+}
+
+// R returns the value of the red channel.
+func (c Color) R() uint8 {
+	return uint8(c & 0xFF0000 >> 16)
+}
+
+// G returns the value of the green channel.
+func (c Color) G() uint8 {
+	return uint8(c & 0xFF00 >> 8)
+}
+
+// B returns the value of the blue channel.
+func (c Color) B() uint8 {
+	return uint8(c & 0xFF)
+}
+
+// A returns the value of the alpha channel.
+func (c Color) A() uint8 {
+	return uint8(c & 0xFF000000 >> 24)
 }
