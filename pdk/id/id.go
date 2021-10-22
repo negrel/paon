9a548b9322495551196b54c18a797062453f50f1
Alexandre Negrel <negrel.dev@protonmail.com>
2021-10-22 23:32:51 +0200

    feat: add Registry type to pdk/id package

--- pdk/id/id.go
+++ pdk/id/id.go
@@ -2,14 +2,26 @@ package id
 
 import "sync/atomic"
 
-// ID define a unique number.
-type ID int32
+// Registry define an identifier registry.
+// All id generated from a registry are unique.
+type Registry struct {
+	counter uint32
+}
 
-var idCounter int32 = 0
-
-// New returns a new ID.
-func New() ID {
-	id := atomic.AddInt32(&idCounter, 1)
-
+// New returns a new unique ID.
+func (r *Registry) New() ID {
+	id := atomic.AddUint32(&r.counter, 1)
 	return ID(id)
+}
+
+// ID define a unique number.
+type ID uint32
+
+var globalReg = Registry{
+	counter: 0,
+}
+
+// New returns a new unique ID from the global Registry.
+func New() ID {
+	return globalReg.New()
 }
