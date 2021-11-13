143d3b6cb3a66508bb607d5fb8594007620d74ca
Alexandre Negrel <negrel.dev@protonmail.com>
2021-11-13 19:06:03 +0100

    refacto: styles/property and styles/values package

---
+++ styles/property/iface.go
@@ -0,0 +1,40 @@
+package property
+
+import (
+	"github.com/negrel/paon/pdk/id"
+)
+
+// IfaceID define a unique ID accross Iface properties.
+type IfaceID id.ID
+
+var (
+	ifaceRegistry = id.Registry{}
+	ifaceMap      = id.NewStringMap()
+)
+
+// NewIfaceID returns a new unique interface property ID.
+func NewIfaceID(name string) IfaceID {
+	id := ifaceRegistry.New()
+	ifaceMap.Set(id, name)
+
+	return IfaceID(id)
+}
+
+// String implements the fmt.Stringer interface.
+func (ii IfaceID) String() string {
+	return ifaceMap.Get(id.ID(ii))
+}
+
+// IfaceIDCount returns the number of IfaceID generated.
+func IfaceIDCount() uint32 {
+	return uint32(ifaceRegistry.Last())
+}
+
+var (
+	_IDBorderCharSet = NewIfaceID("border-charset")
+)
+
+// BorderCharSet returns the IfaceID of the "border-charset" property.
+func BorderCharSet() IfaceID {
+	return _IDBorderCharSet
+}
