e1ff254558fe5d24215f63c2ea1058a97d8c73e3
Alexandre Negrel <alexandre@negrel.dev>
2024-04-30 18:18:10 +0200

    remove render package

--- render/gen.go
+++
@@ -1,4 +0,0 @@
-package render
-
-//go:generate mockgen -destination draw_surface_mock_test.go -package render github.com/negrel/paon/draw Surface
-//go:generate mockgen -source renderable.go -destination renderable_mock_test.go -package render . Renderable
